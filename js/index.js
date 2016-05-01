a97848dce2b6437a705ce533df20feb900a2e0fa
ts1027 <443633568@qq.com>
2016-05-01 21:07:19 +0800

    结构

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+    /*拖拽*/
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 0
+    });
+});
